30bac39b348147b58fa491b9ecaf7ca8a30113c4
controllerha <1193695272@qq.com>
2018-11-29 18:28:08 +0800

    .

--- app.js
+++ app.js
@@ -10,15 +10,20 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var app = express();
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/lvorytower');
+
 //设置跨域
 app.all("*",function(req,res,next){
     //设置允许跨域的域名，*代表允许任意域名跨域
     res.header("Access-Control-Allow-Origin","http://localhost:9000");
     //允许的header类型
-    res.header("Access-Control-Allow-Headers","*");
+    res.header("Access-Control-Allow-Headers","application/x-www-form-urlencoded,application/json,Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    //允许提交cookie
+    res.header("Access-Control-Allow-Credentials", "true");
     //跨域允许的请求方式
     res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
-    if (req.method.toLowerCase() == 'options'){
+    if (req.method.toLowerCase() === 'options'){
         res.send(200);  //让options尝试请求快速结束
     }else{
         next();
@@ -39,9 +44,12 @@ app.use(bodyParser.json());
 //session
 app.set('trust proxy', 1);
 app.use(session({
-    secret: 'Encrypted string',
+    secret: 'xxxxx',
+    name:'lvcookie',
+    cookie: {maxAge: 80000},
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store:require('mongoose-session')(mongoose)
 }));
 
 //加载路由
